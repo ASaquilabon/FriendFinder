b4aaa34c44901dd3181757fb2ca3bae470955881
archie saquilabon <asaquilabon@gmail.com>
2017-12-04 22:07:55 -0800

    added data to friends.js and ports

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,85 @@
-module.exports = friendArray;
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'The Rock',
+		photo: 'http://www.anyfeed.in/wp-content/uploads/2016/08/The-Rock.png',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Stone Cole Steve Austin',
+		photo: 'https://static.independent.co.uk/s3fs-public/styles/article_small/public/thumbnails/image/2016/12/15/11/stone-cold-beer.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Trish Stratus',
+		photo: 'http://frostsnow.com/uploads/gossip/2017/10/30/ron-fisico-and-trish-stratus-wedding-married-and-net-worth.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Beyonce Knowles',
+		photo: 'http://www.billboard.com/files/styles/article_main_image/public/media/beyonce-feb-2014-billboard-650.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Adele Adkins',
+		photo: 'https://i1.imgiz.com/data/videoshots/6829/6829475-6461.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+];
+
+module.exports = friendsArray;
--- server.js
+++ server.js
@@ -7,5 +7,12 @@ var app = express();
 var PORT = process.env.PORT || 8000; 
 
 app.use(bodyParser.json());
+app.use(bodyParser.json({type:"application/vnd.api+json"}));
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text()); 
+app.use(express.static("app"));
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(process.env.PORT || 8000);
